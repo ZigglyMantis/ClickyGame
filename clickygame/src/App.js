2d561dab808defa32291207b30fca4ae18aeba33
Zygimantas Macijauskas <zigglymantis@gmail.com>
2020-01-13 18:18:36 -0500

    pushing up structured

---
+++ clickygame/src/App.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  state = {
+    images: images,
+    score: 0,
+    topScore: 0,
+    guessedImages: [],
+    victoryCondition: ""
+  };
+
+// generate from 0-11 to pick image
+
+// function for guesses up to 11 and wrong guesses
+
+// function for correct guesses all 12
+
+// on click events
+
+// render
+render() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+}
+
+export default App;
