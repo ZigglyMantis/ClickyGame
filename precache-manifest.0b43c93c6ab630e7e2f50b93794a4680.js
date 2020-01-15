96f49b9128a21b37ef74250bf3810c88822a44a4
Zygimantas Macijauskas <zigglymantis@gmail.com>
2020-01-14 21:48:50 -0500

    Updates

---
+++ precache-manifest.0b43c93c6ab630e7e2f50b93794a4680.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63989122a7ded65f60d5bb823ccea5ab",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "73750ab03c6e50c19666",
+    "url": "/ClickyGame/static/css/main.824cbc16.chunk.css"
+  },
+  {
+    "revision": "72a4490dfb8e9609a67e",
+    "url": "/ClickyGame/static/js/2.747bbdea.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ClickyGame/static/js/2.747bbdea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "73750ab03c6e50c19666",
+    "url": "/ClickyGame/static/js/main.f08f7838.chunk.js"
+  },
+  {
+    "revision": "88489d381a86dae2d822",
+    "url": "/ClickyGame/static/js/runtime-main.1386749d.js"
+  }
+]);
