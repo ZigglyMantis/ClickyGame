0f6640cbedba5daceba827cfce135a77d143d8a8
Zygimantas Macijauskas <zigglymantis@gmail.com>
2020-01-14 21:13:38 -0500

    touch ups

--- clickygame/src/components/NavTabs/NavTabs.js
+++ clickygame/src/components/NavTabs/NavTabs.js
@@ -4,13 +4,13 @@ import "./NavTabs.css";
 function NavTabs (props) {
     return (
         <div className="row nav-bar nav-bar-fluid">
-            <div className="col-4">
+            <div className="col-3">
                 <h2>Clicky Game</h2>
             </div>
-            <div className="col-4">
+            <div className="col-6">
                 <h2>{props.victoryCondition}</h2>
             </div>
-            <div className="col-4">
+            <div className="col-3">
                 <h2>Score: {props.score} |-| Wins: {props.Wins}</h2>
             </div>
         </div>
